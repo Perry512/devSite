14e589a59e7808f0d11fc3200f07659f85d93107
Perry512 <perry512@hotmail.com>
2024-06-19 15:38:44 -0400

    [ADD] Component: Adding functionality to test.js

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,16 +1,47 @@
 import React from "react";
 
+import {computePosition} from 'https://cdn.jsdelivr.net/npm/@floating-ui/dom@1.6.5/+esm';
+
 function Test() {
     //const navbar = 'navbar';
+    const testStyle = {
+        background: "#777777",
+        color: "white",
+        padding: "20px",
+        width: "max-content",
+        position: "absolute",
+        top: "0",
+        left: "0"
+
+    }
+
+    const button = document.querySelector('#button');
+    const tooltip = document.querySelector('#tooltip');
+
+    computePosition(button, tooltip).then(({x,y}) => {
+        Object.assign(tooltip.style, {
+            left: `${x}px`,
+            top: `${y}px`,
+
+        });
+    });
 
     return (
-        <nav className='topBar'>
-            <ul>
-                <li> 1 </li>
-                <li> 2 </li>
-                <li> 3 </li>
-            </ul>
-        </nav> 
+        <html>
+            <head> <title> Floating UI </title> </head>
+            
+            <body style={testStyle}>
+                <button id="button" aria-describedby="tooltip">
+                    My Button
+                </button>
+                <div id="tooltip" role="tooltip"> My Tooltip </div>
+
+                <script type="module">
+
+                    
+                </script>
+            </body>
+        </html>
     )
 }
 
