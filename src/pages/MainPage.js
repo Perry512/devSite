233294cdbcbebc34e5195f14ce0d2d1ec481fe46
Perry512 <perry512@hotmail.com>
2024-06-18 20:23:44 -0400

    [ADD] Component: Adding new file structure, adding more pages to site

---
+++ src/pages/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainPage = () => {
+
+    return (
+        <React.Fragment>
+            <head>
+                Hello World!
+            </head>
+            <body> This is the beginning of my resume site! </body>
+        </React.Fragment>
+    );
+
+}
+
+
+export default MainPage;
---
+++ src/pages/Test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Test() {
+    const navbar = 'navbar';
+
+    return (
+        <nav className='topBar'>
+            <ul>
+                <li> 1 </li>
+                <li> 2 </li>
+                <li> 3 </li>
+            </ul>
+        </nav> 
+    )
+}
+
+export default Test;
