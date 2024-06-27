fa1afb7614511ff5f27ebcf203ae173a6309f0da
Perry512 <perry512@hotmail.com>
2024-06-27 19:45:32 -0400

    [ADD] Component: Make slider look better, added counter

--- src/widgets/Slider.tsx
+++ src/widgets/Slider.tsx
@@ -1,57 +1,17 @@
-import React, {  ReactNode } from "react";
-import { useSpring, animated } from '@react-spring/web'
-import { useDrag } from "react-use-gesture";
+import React, { useState } from "react";
 
-import styles from './../styles/styles.module.css';
+import SliderComp from "../components/SliderComp.tsx";
 
-const left = {
-    bg:'#B66A5E',
-    justifySelf: 'end',
-}
-
-const right = {
-    bg: '#3D6567',
-    justifySelf: 'start',
-}
-
-const SliderComp = ({ children}: { children: ReactNode }) => {
-    const [{ x, bg, scale, justifySelf }, api] = useSpring(() => ({
-        x: 0,
-        scale: 1,
-        ...left,
-    }))
-
-    const bind = useDrag(({ active, movement: [x] }) =>
-        api.start({
-            x: active ? x : 0,
-            scale: active ? 1.1 : 1,
-            ...(x < 0 ? left : right),
-            immediate: name => active && name === 'x',
-        })
-    )
-
-    const avSize = x.to({
-        map: Math.abs,
-        range: [50, 300],
-        output: [0.5, 1],
-        extrapolate: 'clamp',
-
-    })
-
-    return (
-        <animated.div {...bind()} className={styles.item} style={{ background: bg }}>
-            <animated.div className={styles.av} style={{scale:avSize, justifySelf }} />
-            <animated.div className={styles.fg} style={{ x, scale }}>
-                {children}
-            </animated.div>
-        </animated.div>
-    )
-}
+import styles from './slider.module.css';
 
 export default function Slider() {
+
+    const [counter, setCounter] = useState(0);
+
     return (
         <div className={styles.container}>
-            <SliderComp>Slide.</SliderComp>
+            <SliderComp setCounter={setCounter}> </SliderComp>
+            <div className={styles.counterDisplay}> Counter: {counter} </div>
         </div>
     )
 }
