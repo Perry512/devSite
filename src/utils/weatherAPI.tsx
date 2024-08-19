e4df56a6c74054a420588256c64a9ae3c4470e45
Perry512 <perry512@hotmail.com>
2024-08-19 18:21:02 -0400

    [FIX] Console: removed log

--- src/utils/weatherAPI.tsx
+++ src/utils/weatherAPI.tsx
@@ -32,7 +32,7 @@ export const getWeather = async(lat: number, lon: number) => {
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${encodeURI(lat.toString())}&longitude=${encodeURI(lon.toString())}&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&past_hours=1&forecast_hours=1`);
     const data = await response.json();
 
-    console.log('city API Response: ', data);
+    // console.log('city API Response: ', data);
 
     const weatherCode = data.hourly.weather_code[0];
     const conditionDirty = wmoCode[weatherCode];
